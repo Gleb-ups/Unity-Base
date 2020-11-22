447b5910a8ea8e86c5ae19d7de511203979cb35a
Gleb-max <52007123+Gleb-max@users.noreply.github.com>
2020-11-22 16:18:24 +0500

    Init

---
+++ Assets/Scripts/ThrowScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowScript : MonoBehaviour
+{
+    public float boxThrust = 10f;
+    public GameObject boxPrefab;
+
+    private Rigidbody mRigidBody;
+    private Vector3 boxPosition;
+    private Quaternion boxRotation;
+    private bool canThrowing;
+    private List<string> tags;
+
+    private void Awake()
+    {
+        mRigidBody = GetComponent<Rigidbody>();
+        mRigidBody.useGravity = false;
+        canThrowing = true;
+        tags = new List<string> { "Wall", "Box", "Floor" };
+    }
+
+    void Start()
+    {
+        boxPosition = transform.position;
+        boxRotation = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canThrowing)
+        {
+            mRigidBody.AddForce(transform.forward * boxThrust);
+            mRigidBody.useGravity = true;
+            canThrowing = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (tags.Contains(collision.gameObject.tag) && !canThrowing)
+        {
+            canThrowing = true;
+            Instantiate(boxPrefab, boxPosition, boxRotation);
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/WallCollision.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class WallCollision : MonoBehaviour
+{
+    [SerializeField]
+    private Material wallMaterial;
+
+    private void Awake()
+    {
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.LogError(collision.gameObject.tag);
+        if(collision.gameObject.tag == "Box")
+        {
+            wallMaterial.color = Random.ColorHSV();
+        }
+    }
+}
